65a0341a73222577037f80dc9128ac1c9043ccf9
lukeinator42 <lukasgrasse@gmail.com>
2022-09-23 10:46:35 -0600

    added argparse

--- poem_reading.py
+++ poem_reading.py
@@ -8,9 +8,16 @@
 
 from poem_states import PoemStateMachine
 from speech_interface import SpeechInterface
+import argparse
 
-with SpeechInterface("0.0.0.0:50052") as speech_interface:
-    state_machine = PoemStateMachine(speech_interface, poem_data_csv="data/poem_data.csv", participant_dest_csv="recordings/test_p0.csv")
+parser = argparse.ArgumentParser(description="Poem Experiment")
+parser.add_argument('--grpc_host', default="0.0.0.0:50052", help="gRPC rero_core address (default 0.0.0.0:50052)")
+parser.add_argument('--dataset_csv', default="data/poem_data.csv", help="path to dataset to run participant on (default data/poem_data.csv)")
+parser.add_argument('--logging_csv', default="recordings/test.csv", help="path to write participant data to (default recordings/test.csv)")
+ARGS = parser.parse_args()
+
+with SpeechInterface(ARGS.grpc_host) as speech_interface:
+    state_machine = PoemStateMachine(speech_interface, poem_data_csv=ARGS.dataset_csv, participant_dest_csv=ARGS.logging_csv)
 
     print("running poem state machine")
     state_machine.run(None)
